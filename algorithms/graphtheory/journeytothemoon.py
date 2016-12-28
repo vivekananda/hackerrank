52c26727e8ffeb58a75eb075df5d3e8227ec9e87
vivekananda tadala <t.vivekananda@gmail.com>
2016-12-28 12:28:07 +0530

    bug fix

--- algorithms/graphtheory/journeytothemoon.py
+++ algorithms/graphtheory/journeytothemoon.py
@@ -12,9 +12,9 @@ class JourneyToTheMoon:
 
     def calculate(self):
         # create groups from known pairs
-        groups = [ set([i]) for i in range(self.n)]
+        groups = [set([i]) for i in range(self.n)]
 
-        for (u,v) in self.known_pairs:
+        for (u, v) in self.known_pairs:
             u_group = None
             v_group = None
             for g in groups:
@@ -24,11 +24,12 @@ class JourneyToTheMoon:
                     v_group = g
             # merge step
             if u_group and v_group:
-                groups.remove(u_group)
-                groups.remove(v_group)
-                groups.append(u_group.union(v_group))
+                if u_group != v_group:
+                    groups.remove(u_group)
+                    groups.remove(v_group)
+                    groups.append(u_group.union(v_group))
             elif not u_group and not v_group:
-                groups.append(set([u,v]))
+                groups.append(set([u, v]))
             elif u_group:
                 groups.remove(u_group)
                 groups.append(u_group.union(set([v])))
@@ -40,19 +41,23 @@ class JourneyToTheMoon:
         total_combinations = 0
         group_counts = [len(g) for g in groups]
         for i in range(len(group_counts)):
-            for j in range(i+1,len(group_counts)):
+            for j in range(i + 1, len(group_counts)):
                 total_combinations += group_counts[i] * group_counts[j]
         return total_combinations
 
 
 class TestJourneyToTheMoon(unittest.TestCase):
     def test1(self):
-        jttm1 = JourneyToTheMoon(4,[(0,1),(2,3)])
-        self.assertEquals(jttm1.calculate(),4)
+        jttm1 = JourneyToTheMoon(4, [(0, 1), (2, 3)])
+        self.assertEquals(jttm1.calculate(), 4)
 
     def test2(self):
         jttm2 = JourneyToTheMoon(10, [(0, 2), (1, 8), (1, 4), (2, 8), (2, 6), (3, 5), (6, 9)])
-        self.assertEquals(jttm2.calculate(),23)
+        self.assertEquals(jttm2.calculate(), 23)
+    
+    def test3(self):
+        jttm3 = JourneyToTheMoon(10, [(0, 2), (2, 8), (2, 6), (1, 8), (1, 4), (3, 5), (8, 6), (6, 9)])
+        self.assertEquals(jttm3.calculate(), 23)
 
 
 if __name__ == "__main__":
@@ -61,5 +66,5 @@ if __name__ == "__main__":
     for i in range(q):
         known_pairs.append([int(x) for x in input().strip().split()])
 
-    jttm = JourneyToTheMoon(n,known_pairs)
+    jttm = JourneyToTheMoon(n, known_pairs)
     print(jttm.calculate())
